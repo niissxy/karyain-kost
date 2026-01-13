de1004149ef81a9afafe6f9aec0be7659a220ff7
niissxy <anisay106@gmail.com>
2026-01-13 10:23:54 +0700

    update

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -17,13 +17,13 @@ class GoogleController extends Controller
         ])
         ->redirect();
 
-        return Socialite::driver('google')->redirect();
+       return Socialite::driver('google')->stateless()->redirect();
     }
 
     public function callback()
     {
         // Ambil user dari Google
-        $googleUser = Socialite::driver('google')->user();
+        $googleUser = Socialite::driver('google')->stateless()->user();
 
         // Update atau buat user baru
         $user = User::updateOrCreate(
