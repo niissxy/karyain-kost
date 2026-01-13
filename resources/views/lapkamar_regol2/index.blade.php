07d520961b00198df907ec9c2d09ae2418dc29d5
niissxy <anisay106@gmail.com>
2026-01-13 16:15:25 +0700

    update lapkamar regol2

--- resources/views/lapkamar_regol2/index.blade.php
+++ resources/views/lapkamar_regol2/index.blade.php
@@ -164,7 +164,7 @@ body {
                 <div class="card-header d-flex justify-content-between align-items-center">
                     <h5 class="mb-0">Laporan Kamar Kost Regol 2</h5>
                     <a href="{{ url('lapkamar_regol2/create') }}"
-                       class="btn btn-success btn-sm">
+                       class="btn btn-warning btn-sm">
                         <i class="bi bi-plus-circle"></i> New
                     </a>
                 </div>
