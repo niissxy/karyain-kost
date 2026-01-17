18923667f5f3b7fc316a91ef04f7d23944e3bca0
niissxy <anisay106@gmail.com>
2026-01-17 11:31:39 +0700

    update margin left layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
         /* ===== MAIN CONTENT ===== */
         #main-content {
-            margin-left: 260px;
+            margin-left: 100px;
             padding: 20px;
             transition: margin-left 0.3s ease;
         }
