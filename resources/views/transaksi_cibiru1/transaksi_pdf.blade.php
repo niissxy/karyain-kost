adaf7645a74902622f3dc4205d215df28927d68a
niissxy <anisay106@gmail.com>
2026-01-17 12:01:56 +0700

    update font

--- resources/views/transaksi_cibiru1/transaksi_pdf.blade.php
+++ resources/views/transaksi_cibiru1/transaksi_pdf.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invoice Transaksi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.bunny.net/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.bunny.net/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
 
     <style>
         /* General Styles */
