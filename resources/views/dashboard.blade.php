52391bced987607606156e5feaf35131d6da8b7c
niissxy <anisay106@gmail.com>
2026-01-17 15:56:12 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -54,7 +54,7 @@ body {
 }
 </style>
 
-<div class="container mt-4" style="margin-left: 80px;">
+<div class="container mt-4 main-content" style="margin-left: 80px;">
 
     <h3 class="mb-4">Dashboard {{ $kostDisplayName }}</h3>
 
