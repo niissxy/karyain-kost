8d2c7244f682249e8288601cba5a7b54ffd75b87
niissxy <anisay106@gmail.com>
2026-01-17 10:07:27 +0700

    margin left nama penghuni

--- resources/views/checkin_cibiru1/checkin_pdf.blade.php
+++ resources/views/checkin_cibiru1/checkin_pdf.blade.php
@@ -138,7 +138,7 @@
             </div>
             <div class="row">
                 <div class="col-sm-6">
-                    <strong>Nama Penghuni</strong> <strong style="margin-left: 50px;"> : </strong>
+                    <strong>Nama Penghuni</strong> <strong style="margin-left: 55px;"> : </strong>
                     <span>{{ $checkin->nama_penghuni }}</span>                   
                 </div>
                 <div class="col-sm-6">
