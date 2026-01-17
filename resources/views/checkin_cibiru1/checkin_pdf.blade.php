4d1bea02a1cd2b536ba3d0425b487ea4b9f45569
niissxy <anisay106@gmail.com>
2026-01-17 10:35:48 +0700

    font invoice

--- resources/views/checkin_cibiru1/checkin_pdf.blade.php
+++ resources/views/checkin_cibiru1/checkin_pdf.blade.php
@@ -9,7 +9,7 @@
     <style>
         /* General Styles */
         body {
-            font-family: 'Monserrat', sans-serif;
+            font-family: 'Montserrat', sans-serif;
             background-color: #f4f4f9;
             color: #333;
             padding: 40px;
