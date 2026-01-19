97d6fed904b2e39af38d1cc946b365fc718624c8
niissxy <anisay106@gmail.com>
2026-01-19 08:55:28 +0700

    add notification login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,12 +75,12 @@
                             <label class="form-check-label" for="remember">Remember Me</label>
                         </div>
 
-                        <button type="submit" class="btn btn-primary w-100 py-2 fw-semibold">
+                        <button type="submit" id="btn-login" class="btn btn-primary w-100 py-2 fw-semibold">
                             Login
                         </button>
 
                         <div class="text-center mt-3">
-                            <a href="{{ route('register') }}" class="fw-semibold">Register</a><br>
+                            <a href="{{ route('register') }}" id="btn-register" class="fw-semibold">Register</a><br>
                             <a href="{{ route('password.request') }}" class="text-muted small">
                                 Lupa Password?
                             </a>
@@ -94,3 +94,92 @@
     </div>
 </div>
 @endsection
+
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function() {
+
+        $(".btn-login").click(function() {
+
+            var name = $("#name").val();
+            var password = $("#password").val();
+            var token = $("meta[name='csrf-token']").attr("content");
+
+            if (name.length == "") {
+
+                Swal.fire({
+                    type: 'warning',
+                    title: 'Oops...',
+                    text: 'Name Wajib Diisi!'
+                });
+
+            } else if (password.length == "") {
+
+                Swal.fire({
+                    type: 'warning',
+                    title: 'Oops...',
+                    text: 'Password Wajib Diisi!'
+                });
+
+            } else {
+
+                $.ajax({
+
+                    url: "{{ url('login') }}",
+                    type: "POST",
+                    dataType: "JSON",
+                    cache: false,
+                    data: {
+                        "name": name,
+                        "password": password,
+                        "_token": token
+                    },
+
+                    success: function(response) {
+
+                        if (response.success) {
+
+                            Swal.fire({
+                                    type: 'success',
+                                    title: 'Login Berhasil!',
+                                    text: 'Anda akan diarahkan dalam 3 Detik',
+                                    icon: 'success',
+                                    timer: 3000,
+                                    showCancelButton: false,
+                                    showConfirmButton: false
+                                })
+                                .then(function() {
+                                    window.location.href =
+                                        "{{ route('dashboard') }}";
+                                });
+
+                        }
+                    },
+
+                    error: function(response) {
+
+                        Swal.fire({
+                                type: 'error',
+                                title: 'Login Gagal!',
+                                text: 'Username / Password Salah'
+                            });
+
+                        console.log(response);
+
+                    }
+
+                });
+
+            }
+
+        });
+
+        $(document).on("keypress", function(e) {
+            if (e.which == 13) {
+                $("#btn-login").click();
+            }
+        });
+
+    });
+</script>
