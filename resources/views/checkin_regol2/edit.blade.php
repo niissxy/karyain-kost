f40b76bcc18ce4f5750504b1274632a02af3fad5
niissxy <anisay106@gmail.com>
2026-02-05 15:23:51 +0700

    update edit checkin

--- resources/views/checkin_regol2/edit.blade.php
+++ resources/views/checkin_regol2/edit.blade.php
@@ -64,20 +64,6 @@
                             <div class="row mb-3">
                                 <label class="col-sm-3 col-form-label">Tanggal Check Out</label>
                                 <div class="col-sm-9">
-                                    <input type="date" class="form-control" value="{{ old('tgl_checkout',$checkin_regol2->tgl_checkout) }}"  name="tgl_checkout"  required autofocus>
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label class="col-sm-3 col-form-label">Jam Check In</label>
-                                <div class="col-sm-9">
-                                    <input type="time" class="form-control" value="{{ old('jam_checkin',$checkin_regol2->jam_checkin) }}"  name="jam_checkin"  required autofocus>
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label class="col-sm-3 col-form-label">Tanggal Check Out</label>
-                                <div class="col-sm-9">
                                     <input type="time" class="form-control" value="{{ old('jam_checkout',$checkin_regol2->jam_checkout) }}"  name="tgl_checkout"  required autofocus>
                                 </div>
                             </div>
